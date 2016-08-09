4c5318f6f4bc8c410d5df8ecb31c90f9795fc65f
CodeSoldier <macauley0924@gmail.com>
2016-08-09 14:39:43 +0800

    Version0.9
    优化：代码优化，程序更完善更健壮。
    Bug修复：解决Version0.8版本的EOFException问题

--- src/com/chatsystem/view/ChatServer.java
+++ src/com/chatsystem/view/ChatServer.java
@@ -8,23 +8,40 @@ public class ChatServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean started = false;
+		ServerSocket ss = null;
+		Socket s = null;
+		DataInputStream dis = null;
 		try{
-			ServerSocket ss = new ServerSocket(8888);
+			ss = new ServerSocket(8888);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		try{
 			started = true;
 			while(started){
 				boolean bConnected = false;
-				Socket s = ss.accept();
+				s = ss.accept();
 System.out.println("A Client Connected!");				
 				bConnected = true;
-				DataInputStream dis = new DataInputStream(s.getInputStream());
+				dis = new DataInputStream(s.getInputStream());
 				while(bConnected){
 					String str = dis.readUTF();
 					System.out.println(str);
 				}
-				dis.close();
+//				dis.close();
 			}
-		}catch(IOException e){
-			e.printStackTrace();
+		}catch(Exception e){
+//			e.printStackTrace();
+			System.out.println("Client Closed!");
+		}finally{
+			try {
+				if(dis != null) dis.close();
+				if(s != null) s.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
 		}
 	}
 

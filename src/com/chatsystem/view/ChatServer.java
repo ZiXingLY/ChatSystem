e6cb9ca7f61a8c787bdc660793607711f2e8b1c9
CodeSoldier <macauley0924@gmail.com>
2016-08-09 15:29:42 +0800

    Version1.0
    添加多客户端，（为了练习多线程，所以采用多线程方式解决多客户端问题，实际可以用异步IO）

--- src/com/chatsystem/view/ChatServer.java
+++ src/com/chatsystem/view/ChatServer.java
@@ -4,14 +4,53 @@ import java.io.*;
 import java.net.*;
 
 public class ChatServer {
+	boolean started = false;
+	ServerSocket ss = null;
+	
+	class Client implements Runnable {
+		private Socket s;
+		private DataInputStream dis = null;
+		private boolean bConnected = false;
 		
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean started = false;
-		ServerSocket ss = null;
-		Socket s = null;
-		DataInputStream dis = null;
+		public Client(Socket s){
+			this. s = s;
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				bConnected = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try{
+				while(bConnected){
+					String str = dis.readUTF();
+					System.out.println(str);
+				}
+			}catch(EOFException e){
+				System.out.println("Client Closed!");			
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				try {
+					if(dis != null) dis.close();
+					if(s != null) s.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		}
+		
+	}
+	
+	public void start(){
 		try{
+			started = true;
 			ss = new ServerSocket(8888);
 		}catch(BindException e){
 			System.out.println("端口正在被使用！");
@@ -21,33 +60,26 @@ public class ChatServer {
 			e.printStackTrace();
 		}
 		try{
-			started = true;
 			while(started){
-				boolean bConnected = false;
-				s = ss.accept();
-System.out.println("A Client Connected!");				
-				bConnected = true;
-				dis = new DataInputStream(s.getInputStream());
-				while(bConnected){
-					String str = dis.readUTF();
-					System.out.println(str);
-				}
-//				dis.close();
+				Socket s = ss.accept();
+				Client c = new Client(s);
+System.out.println("A Client Connected!");	
+				new Thread(c).start();
 			}
-		}catch(EOFException e){
-			System.out.println("Client Closed!");			
-		}catch(Exception e){
+		}catch(IOException e){
 			e.printStackTrace();
 		}finally{
 			try {
-				if(dis != null) dis.close();
-				if(s != null) s.close();
-			} catch (IOException e1) {
+				ss.close();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
-			
 		}
 	}
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ChatServer().start();
+	}
+	
 }

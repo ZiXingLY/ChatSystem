a6fb8c268df142bc0434a7d8c4fd672715c21412
CodeSoldier <macauley0924@gmail.com>
2016-08-09 09:55:45 +0800

    Version0.3
    为窗体添加关闭事件

--- src/com/chatsystem/view/ChatClient.java
+++ src/com/chatsystem/view/ChatClient.java
@@ -2,21 +2,32 @@ package com.chatsystem.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class ChatClient extends Frame{
+public class ChatClient extends Frame {
 	TextField textField = new TextField();
 	TextArea textArea = new TextArea();
-	
-	public void launchFrame(){
-		this.setLocation(400,300);
+
+	public void launchFrame() {
+		this.setLocation(400, 300);
 		this.setSize(300, 300);
 		this.add(textField, BorderLayout.SOUTH);
 		this.add(textArea, BorderLayout.NORTH);
 		pack();
+		this.addWindowListener(new WindowAdapter(){
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+			
+		});
 		this.setVisible(true);
-//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new ChatClient().launchFrame();
